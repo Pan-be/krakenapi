2837dbee42433bbbab591baeb9fed2d60e59fc95
Pan.Be <konradbieniek@gmail.com>
2026-02-11 22:33:30 +0100

    public directory

---
+++ public/candles.php
@@ -0,0 +1,27 @@
+<?php
+// ini_set('memory_limit', '1024M');
+// ini_set('memory_limit', '2048M');
+ini_set('memory_limit', '3072M');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../autoload.php';
+
+use Core\ErrorHandler;
+use Core\Config;
+use Core\Request;
+use Controllers\CandleFetcherController;
+
+set_exception_handler([ErrorHandler::class, 'handleException']);
+
+header('Content-Type: application/json');
+
+$allowedIntervals = Config::load('allowedIntervals');
+
+$interval = Request::getIntervalFromQuery($allowedIntervals);
+
+$controller = new CandleFetcherController();
+$data = $controller->candleHandle($interval);
+
+echo json_encode($data, JSON_PRETTY_PRINT);
